4206d5e64c1c53ace37011de3abd1b5a4f106416
Mahesh Chandra <mmahesh.chandra873@gmail.com>
2021-06-12 11:26:34 +0530

    shell script cleanups

--- poisson_linear_inverse_problems/generate_plots.sh
+++ poisson_linear_inverse_problems/generate_plots.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-python create_plots.py --fun_num=0 
+# here use actual fun_num - 1  to generate plots
+python create_plots.py --fun_num=0  
 python create_plots.py --fun_num=1 
 python create_plots.py --fun_num=2
--- robust_phase_retrieval/generate_results.sh
+++ robust_phase_retrieval/generate_results.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+## ignore 1,5 algos as they are related to cocain bpg
+
 # python main.py --fun_num=1 --abs_fun_num=1 --algo=1
 python main.py --fun_num=1 --abs_fun_num=1 --algo=2
 python main.py --fun_num=1 --abs_fun_num=1 --algo=3
@@ -11,4 +13,3 @@ python main.py --fun_num=2 --abs_fun_num=2 --algo=3
 python main.py --fun_num=2 --abs_fun_num=2 --algo=4
 # python main.py --fun_num=2 --abs_fun_num=2 --algo=5
 
-## ignore 1,5 algos
